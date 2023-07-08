58960cd55cf2689fe057820ec58c042453fcd0da
11mouni <138847085+11mouni@users.noreply.github.com>
2023-07-08 16:14:35 +0530

    commin_5

--- Java/src/Second/pract2.java
+++ Java/src/Second/pract2.java
@@ -3,10 +3,10 @@ package Second;
 public class pract2 {
 
 	public static void main(String[] args) {
-	System.out.println("practicingg the git ");
-	System.out.println("new commit");
-	System.out.println("commit # 3");
-
+		System.out.println("practicingg the git ");
+		System.out.println("new commit");
+		System.out.println("from Branch3");
+		System.out.println("update");
 	}
 
 }
