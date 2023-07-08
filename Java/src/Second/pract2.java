ff97b3be9d8003193c15fc3b983f85834ffdf7b8
11mouni <138847085+11mouni@users.noreply.github.com>
2023-07-08 16:09:27 +0530

    commit_3

--- Java/src/Second/pract2.java
+++ Java/src/Second/pract2.java
@@ -5,6 +5,7 @@ public class pract2 {
 	public static void main(String[] args) {
 	System.out.println("practicingg the git ");
 	System.out.println("new commit");
+	System.out.println("commit # 3");
 
 	}
 
