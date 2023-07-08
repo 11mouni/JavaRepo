6c09d0d29cf4e57d7367facf8081420d8ade28d1
11mouni <138847085+11mouni@users.noreply.github.com>
2023-07-08 12:01:42 +0530

    new_CC_commit
    new_ CC_ commit

--- Java/src/Second/gttt.java
+++ Java/src/Second/gttt.java
@@ -3,9 +3,8 @@ package Second;
 public class gttt {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("Hik,");
+		
+		System.out.println("hello practicee");
 	}
 
 }
